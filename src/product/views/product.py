87a5ab405a458957044e0aeaa0714c3be6ecf8d9
joy <jofficial248@gmail.com>
2022-11-12 19:44:41 +0600

    changes in product.py, list.html,urls.py

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant,Product
 
 
 class CreateProductView(generic.TemplateView):
@@ -12,3 +12,9 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+class ProductListView(generic.ListView):
+    context_object_name = 'products'
+    model = Product
+    template_name = 'products/list.html'
+
